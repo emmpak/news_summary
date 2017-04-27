f078f5cf735ee6f942bb7dbf88b2b91c1189b6bb
Will Schwier <w.schwier@hotmail.co.uk>
2017-04-27 14:34:46 +0100

    Populate array with article

--- spec/articleListSpec.js
+++ spec/articleListSpec.js
@@ -2,3 +2,15 @@
   var articleList = new ArticleList()
     assert.arraysEqual(articleList.getArticles(),[]);
 })();
+
+(function() {
+  var articleList = new ArticleList()
+  articleList.saveArticles(response())
+  assert.isTrue(articleList.articles.length === 10)
+})();
+
+(function() {
+  var articleList = new ArticleList()
+  articleList.saveArticles(response())
+  assert.isEqual(articleList.articles[0].title, "General election 2017: Tony Blair refuses to endorse Jeremy Corbyn for prime minister – politics live")
+})();
--- src/articleListModel.js
+++ src/articleListModel.js
@@ -4,7 +4,12 @@
   };
 
 ArticleList.prototype = {
-  getArticles: function() { return this.articles }
+  getArticles: function() { return this.articles },
+  saveArticles: function(input) {
+    for( var i = 0; i < input.response.results.length; i ++)
+      this.articles.push(new Article(input.response.results[i].webTitle,
+                                     input.response.results[i].fields.body))
+  }
 }
 
 exports.ArticleList = ArticleList;
